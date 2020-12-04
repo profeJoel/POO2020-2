3ec0ac9747faf73417a114f98566ef0fc6c74c05
profe Joel <joel.torres@ulagos.cl>
2020-12-04 09:45:49 -0300

    preparacion clase 20

---
+++ UNIDAD_0/otros/Starfish/StarfishCollector.java
@@ -0,0 +1,26 @@
+package Starfish;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StarfishCollector extends Application {
+    Stage ventana;
+
+    public static void main(String[] args) {
+        try {
+            launch(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ventana = primaryStage;
+        ventana.setT
+    }
+
+
+}
