37f253752ff611f91064d17d449df8e086afb944
profe Joel <joel.torres@ulagos.cl>
2020-12-16 12:35:46 -0300

    Preparando el juego con Sprite

--- UNIDAD_0/otros/JuegoSprite/Juego.java
+++ UNIDAD_0/otros/JuegoSprite/Juego.java
@@ -1,8 +1,8 @@
 package JuegoSprite;
 
+import javafx.application.Application;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.application.Application;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -22,13 +22,11 @@ import javafx.util.Duration;
 
 public class Juego extends Application{
     Stage ventana;
-    public static double ANCHO = 1050, ALTO = 800;
+    public static double ANCHO = 850, ALTO = 480;
     double puntaje = 0;
 
-    Emoji e1 = new Emoji("e1.png", 50, 50, 100, 100, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP, KeyCode.DOWN);
-    Emoji e2 = new Emoji("e2.png", 525 - 50,  400 - 50, 100, 100, KeyCode.A, KeyCode.D, KeyCode.W, KeyCode.S);
-
-    Image fondo = new Image(getClass().getResourceAsStream("pizarra.jpg"));
+    Image fondo = new Image(getClass().getResourceAsStream("img/bg.jpg"));
+    public Personaje heroe = new Personaje((int) ANCHO/2, (int) ALTO/2, "heroe", KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP, KeyCode.DOWN, KeyCode.SPACE, KeyCode.X);
 
     public static void main(String[] args) {
         launch(args);
@@ -48,61 +46,20 @@ public class Juego extends Application{
         ventana = primaryStage;
         Scene escena = new Scene(grupo);
 
-        //Captura de evento de mouse
-        lienzo.setOnMouseMoved(e -> e1.setXYCentrado(e.getX(), e.getY()));
-        
-        //Captura evento de teclado
-        /*escena.setOnKeyPressed(e -> {
-            if(e.getCode() == KeyCode.LEFT)
-                e1.disminuirX(5);
-            if(e.getCode() == KeyCode.RIGHT)
-                e1.aumentarX(5);
-            if(e.getCode() == KeyCode.UP)
-                e1.disminuirY(5);
-            if(e.getCode() == KeyCode.DOWN)
-                e1.aumentarY(5);
-        });*/
+        //lienzo.setOnMouseMoved(e -> );
 
-        escena.setOnKeyPressed(e -> {
-            e1.mover(e);
-            //e2.mover(e);
-        });
+        escena.setOnKeyPressed(e -> heroe.capturarMovimiento(e));
 
-        ventana.setTitle("Ejemplo de Canvas");
+        ventana.setTitle("Ejemplo con Sprites");
         ventana.setScene(escena);
         ventana.show();
         reloj.play();
     }
 
     private void dibujar(GraphicsContext gc){
-        /*
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, 1050, 800);
-        gc.setFill(Color.BLUE);
-        gc.fillRect(x, y, 100, 100);
-        */
-        e2.moverSolo();
-
         gc.drawImage(fondo, 0, 0, ANCHO, ALTO);
-        e1.dibujar(gc);
-        e2.dibujar(gc);
-        //gc.drawImage(e1.getImagen(), e1.getX(), e1.getY(), e1.getAncho(), e1.getAlto());
-        //gc.drawImage(e2.getImagen(), e2.getX(), e2.getY(), e2.getAncho(), e2.getAlto());
-        
-        if(e1.colisionaCon(e2)){
-            //System.out.println("\n\nColisiona");
-            gc.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.ITALIC, 48.0));
-            gc.setFill(Color.RED);
-            gc.fillText("COLISION", 10, 50);
-
-            puntaje += 1;
-            //System.out.println("e1 > " + e1.getPuntoIzquierda() + ", " + e1.getPuntoDerecha() + ", " + e1.getPuntoArriba() + ", " + e1.getPuntoAbajo());
-            //System.out.println("e2 > " + e2.getPuntoIzquierda() + ", " + e2.getPuntoDerecha() + ", " + e2.getPuntoArriba() + ", " + e2.getPuntoAbajo());
-        }
-        
-        gc.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.REGULAR, 32.0));
-        gc.setFill(Color.YELLOW);
-        gc.fillText("PUNTAJE: " + (int) puntaje, 750, 50);
+        heroe.mover();
+        heroe.dibujar(gc);
     }
 
 }
--- UNIDAD_0/otros/JuegoSprite/Personaje.java
+++ UNIDAD_0/otros/JuegoSprite/Personaje.java
@@ -1,5 +1,209 @@
 package JuegoSprite;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+
 public class Personaje {
-    
+    private int x, y, ancho, alto, velocidad, impulso, contadorPasos, salud, margen, puntoAbajo, puntoArriba, puntoIzquierdo, puntoDerecho;
+    private int [] camino = new int[2];
+    private int [] zonaImpacto = new int[4];
+    private boolean haSaltado, vaIzquierda, vaDerecha, vaAbajo, vaArriba;
+    private Image [] caminaIzquierda;
+    private Image [] caminaDerecha;
+    private Image quieto;
+    private KeyCode l, r, u, d, jump, shot;
+
+    public Personaje(int x, int y, String fuente, KeyCode l, KeyCode r, KeyCode u, KeyCode d, KeyCode jump, KeyCode shot){
+        this.x = x;
+        this.y = y;
+        this.velocidad = 5;
+        this.impulso = 10;
+        this.contadorPasos = 0;
+        this.haSaltado = false;
+        this.vaIzquierda = false;
+        this.vaDerecha = false;
+        this.vaAbajo = false;
+        this.vaArriba = false;
+        this.salud = 10;
+
+        int cantidadImagenes = 9;
+        this.caminaIzquierda = new Image [cantidadImagenes];
+        this.caminaDerecha = new Image [cantidadImagenes];
+        for(int i = 0; i < cantidadImagenes; i++){
+            this.caminaIzquierda[i] = new Image(getClass().getResourceAsStream("img/" + fuente + "/L" + (i+1) + ".png"));
+            this.caminaDerecha[i] = new Image(getClass().getResourceAsStream("img/" + fuente + "/R" + (i+1) + ".png"));
+        }
+        this.quieto = new Image(getClass().getResourceAsStream("img/" + fuente + "/standing.png"));
+        this.ancho = (int) this.quieto.getWidth();
+        this.alto = (int) this.quieto.getHeight();
+
+        this.setZonaImpacto();
+        this.l = l;
+        this.r = r;
+        this.u = u;
+        this.d = d;
+        this.jump = jump;
+        this.shot = shot;
+        System.out.println("Listo: " + fuente);
+    }
+
+    private void setCamino(int limite){
+        this.camino[0] = this.x;
+        this.camino[1] = limite;
+    }
+
+    private void setZonaImpacto(int x, int y, int ancho, int alto){
+        this.zonaImpacto[0] = x + this.margen;
+        this.zonaImpacto[1] = y + this.margen;
+        this.zonaImpacto[2] = ancho - this.margen;
+        this.zonaImpacto[3] = alto - this.margen;
+    }
+
+    private void setZonaImpacto(){
+        this.zonaImpacto[0] = this.x + this.margen;
+        this.zonaImpacto[1] = this.y + this.margen;
+        this.zonaImpacto[2] = this.ancho - this.margen;
+        this.zonaImpacto[3] = this.alto - this.margen;
+        this.setPuntos();
+    }
+
+    public void dibujar(GraphicsContext gc){
+        if(this.contadorPasos + 1 > 27)
+            this.contadorPasos = 0;
+
+        if(this.vaIzquierda){
+            gc.drawImage(this.caminaIzquierda[(int) this.contadorPasos/3], this.x, this.y, this.ancho, this.ancho);
+            this.contadorPasos++;
+        }
+        else if(this.vaDerecha){
+            gc.drawImage(this.caminaDerecha[(int) this.contadorPasos/3], this.x, this.y, this.ancho, this.ancho);
+            this.contadorPasos++;
+        }
+        else
+            gc.drawImage(this.quieto, this.x, this.y, this.ancho, this.ancho);
+        
+        this.setZonaImpacto();   
+        this.dibujarSalud(gc); 
+    }
+
+    private void dibujarSalud(GraphicsContext gc){
+        gc.setFill(Color.rgb(255, 0, 0));
+        gc.fillRect(this.x, this.y - 20, this.ancho, 10);
+
+        gc.setFill(Color.rgb(0, 128, 0));
+        gc.fillRect(this.x, this.y - 20, this.ancho - (5 * (10 - this.salud)), 10);
+    }
+
+    public void capturarMovimiento(KeyEvent e){
+        if(e.getCode() == this.l){
+            this.vaIzquierda = true;
+            this.vaDerecha = false;
+        }
+        else if(e.getCode() == this.r){
+            this.vaDerecha = true;
+            this.vaIzquierda = false;
+        }
+        else if(e.getCode() == this.u && this.y > this.velocidad)
+            this.y -= this.velocidad;
+        else if(e.getCode() == this.d && this.y + this.alto + this.velocidad < Juego.ALTO)
+            this.y += this.velocidad;
+        else if(e.getCode() == this.jump){
+            this.haSaltado = true;
+            this.vaIzquierda = false;
+            this.vaDerecha = false;
+            this.contadorPasos = 0;
+        }
+        else{
+            this.vaIzquierda = false;
+            this.vaDerecha = false;
+            this.contadorPasos = 0;
+        }
+    }
+
+    public void mover(){
+        if(this.vaIzquierda && this.x > this.velocidad){
+            this.x -= this.velocidad;
+            this.vaIzquierda = true;
+            this.vaDerecha = false;
+        }
+        else if(this.vaDerecha && this.x + this.ancho + this.velocidad < Juego.ANCHO){
+            this.x += this.velocidad;
+            this.vaDerecha = true;
+            this.vaIzquierda = false;
+        }
+        else{
+            this.vaIzquierda = false;
+            this.vaDerecha = false;
+            this.contadorPasos = 0;
+        }
+
+        if(this.haSaltado){
+            if(this.impulso >= -10){
+                if(this.impulso < 0)
+                    this.y = (int) ((int) (this.impulso * this.impulso) * 0.5 * -1);
+                else
+                    this.y = (int) ((int) (this.impulso * this.impulso) * 0.5);
+                this.impulso -= 1;
+            }
+            else{
+                this.haSaltado = false;
+                this.impulso = 10;
+            }
+        }
+        else{
+            if(this.vaArriba && this.y > this.velocidad)
+                this.y -= this.velocidad;
+            if(this.haSaltado && this.y + this.alto + this.velocidad < Juego.ALTO)
+                this.y += this.velocidad;
+            if(this.haSaltado){
+                this.haSaltado = true;
+                this.vaIzquierda = false;
+                this.vaDerecha = false;
+                this.contadorPasos = 0;
+            }
+        }
+    }
+
+    public double getPuntoIzquierdo(){
+        return this.puntoIzquierdo;
+    }
+    public double getPuntoDerecho(){
+        return this.puntoDerecho;
+    }
+
+    public double getPuntoArriba(){
+        return this.puntoArriba;
+    }
+    public double getPuntoAbajo(){
+        return this.puntoAbajo;
+    }
+
+    private void setPuntos(){
+        this.puntoAbajo = this.zonaImpacto[1] + this.zonaImpacto[3];
+        this.puntoArriba = this.zonaImpacto[1];
+        this.puntoIzquierdo = this.zonaImpacto[0];
+        this.puntoDerecho = this.zonaImpacto[0] + this.zonaImpacto[2];
+    }
+
+    public boolean colisionaCon(Personaje p){
+        return (this.puntoDerecho > p.getPuntoIzquierdo() && this.puntoIzquierdo < p.getPuntoDerecho() &&
+                    this.puntoArriba < p.getPuntoAbajo() && this.puntoAbajo > p.getPuntoArriba());
+    }
+
+    public void esGolpeado(){
+        this.haSaltado = false;
+        this.impulso = 10;
+        this.x = 100;
+        this.y = 410;
+        this.contadorPasos = 0;
+        this.salud -= 5;
+        //ver como pausar el reloj
+    }
+
+
+
+
 }
